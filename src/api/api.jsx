280bc1c7730cbceeae6012c79212496665a22f0d
DogonYu <dorogon01@gmail.com>
2021-03-23 15:37:09 +0900

    edit: api 파라미터 보내는 방식 수정

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -2,21 +2,23 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://problem.comento.kr',
+  headers: {
+    'Accept': 'application/json',
+  },
 });
 
 export const readFeeds = ({ page, ord, category, limit }) => {
-  return API.get(`/api/list`, { page, ord, category, limit });
+  return API.get(`/api/list`, { params: { page, ord, category, limit } });
 };
 
-export const readFilterCategory = () => {
+export const readFilterCategorys = () => {
   return API.get(`/api/category`);
 };
 
 export const readAds = ({ page, limit }) => {
-  // 이미지 경로 : https://cdn.comento.kr/assignment/파일명
-  return API.get(`/api/ads`, { page, limit });
+  return API.get(`/api/ads`, { params: { page, limit } });
 };
 
 export const readDetailFeed = ({ id }) => {
-  return API.get(`/api/view`, { id });
+  return API.get(`/api/view`, { params: { id } });
 };
