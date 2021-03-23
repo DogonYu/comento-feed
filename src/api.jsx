55c32c30b9151d74a0bf82affbbfe760a780be30
DogonYu <dorogon01@gmail.com>
2021-03-23 11:48:23 +0900

    add: api 정의

---
+++ src/api.jsx
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'https://problem.comento.kr',
+});
+
+export const readFeeds = ({ page, ord, category, limit }) => {
+  return API.get(`/api/list`, { page, ord, category, limit });
+};
+
+export const filterCategory = () => {
+  return API.get(`/api/category`);
+};
+
+export const readAds = ({ page, limit }) => {
+  // 이미지 경로 : https://cdn.comento.kr/assignment/파일명
+  return API.get(`/api/ads`, { page, limit });
+};
+
+export const readDetailFeed = ({ id }) => {
+  return API.get(`/api/view`, { id });
+};
