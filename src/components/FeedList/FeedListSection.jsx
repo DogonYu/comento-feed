c2464f67274815c25977bfd6e33d2297278731d3
DogonYu <dorogon01@gmail.com>
2021-03-23 19:06:27 +0900

    edit: 반응형

--- src/components/FeedList/FeedListSection.jsx
+++ src/components/FeedList/FeedListSection.jsx
@@ -23,53 +23,57 @@ const FeedListSection = () => {
     <section className="feedlist-section">
       {categorys && <FeedListOption categorys={categorys} />}
 
-      {feeds.data.length > 0 &&
-        ads.data.length > 0 &&
-        categorys &&
-        feeds.data.map(feed => {
-          const createdAt = replaceDateFormat(feed.created_at);
-          return (
-            <div className="feed" key={`feeds-${feed.id}`}>
-              <div className="feed-link">
-                <Link to={`/view/${feed.id}`}>
-                  <div className="feed-item">
-                    <div className="feed-item-category">
-                      <div className="feed-item-category-name">
-                        {categorys.category.filter(category => category.id === feed.category_id)[0].name}
+      <div className="feed-wrap">
+        {feeds.data.length > 0 &&
+          ads.data.length > 0 &&
+          categorys &&
+          feeds.data.map(feed => {
+            const createdAt = replaceDateFormat(feed.created_at);
+            return (
+              <div className="feed" key={`feeds-${feed.id}`}>
+                <div className="feed-link">
+                  <Link to={`/view/${feed.id}`}>
+                    <div className="feed-item">
+                      <div className="feed-item-category">
+                        <div className="feed-item-category-name">
+                          {categorys.category.filter(category => category.id === feed.category_id)[0].name}
+                        </div>
+                        <div className="feed-item-category-id">{feed.id}</div>
                       </div>
-                      <div className="feed-item-category-id">{feed.id}</div>
+                      <div className="feed-item-info">
+                        <div className="feed-item-info-userId">{feed.user_id}</div>
+                        <div className="separator" />
+                        <div className="feed-item-info-create">{createdAt}</div>
+                      </div>
+                      <div className="feed-item-content">
+                        <div className="feed-item-title">{feed.title}</div>
+                        <div className="feed-item-description">{feed.contents}</div>
+                      </div>
                     </div>
-                    <div className="feed-item-info">
-                      <div className="feed-item-info-userId">{feed.user_id}</div>
-                      <div className="separator" />
-                      <div className="feed-item-info-create">{createdAt}</div>
-                    </div>
-                    <div className="feed-item-content">
-                      <div className="feed-item-title">{feed.title}</div>
-                      <div className="feed-item-description">{feed.contents}</div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-              {feed.id % 3 === 0 ? (
-                <div key={`ads-${ads.data[feed.id].id}`} className="feed-ads">
-                  <div className="feed-ads-category">sponsored</div>
-                  <div className="feed-ads-content">
-                    <img
-                      className="feed-ads-image"
-                      src={`https://cdn.comento.kr/assignment/${ads.data[feed.id].img}`}
-                      alt=""
-                    />
-                    <div className="feed-ads-text">
-                      <div className="feed-ads-title">{ads.data[feed.id].title}</div>
-                      <div className="feed-ads-description">{ads.data[feed.id].contents}</div>
-                    </div>
-                  </div>
+                  </Link>
                 </div>
-              ) : null}
-            </div>
-          );
-        })}
+                {feed.id % 3 === 0 ? (
+                  <div key={`ads-${ads.data[feed.id].id}`} className="feed-ads">
+                    <div className="feed-ads-category">sponsored</div>
+                    <div className="feed-ads-content">
+                      <div className="feed-ads-image-wrap">
+                        <img
+                          className="feed-ads-image"
+                          src={`https://cdn.comento.kr/assignment/${ads.data[feed.id].img}`}
+                          alt=""
+                        />
+                      </div>
+                      <div className="feed-ads-text">
+                        <div className="feed-ads-title">{ads.data[feed.id].title}</div>
+                        <div className="feed-ads-description">{ads.data[feed.id].contents}</div>
+                      </div>
+                    </div>
+                  </div>
+                ) : null}
+              </div>
+            );
+          })}
+      </div>
 
       {isMount && <div ref={target} />}
     </section>
